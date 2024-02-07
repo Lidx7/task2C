e0f7b2693d300b429bf82f0d26d3d90892e516c9
lidor <lidor.borofker@windowslive.com>
2024-02-07 22:40:38 +0200

    new file:   my_graph.c 	renamed:    my_graph -> my_mat.h

--- my_mat.h
+++ my_mat.h
@@ -1,14 +1,15 @@
-#define J=10
 #include <stdio.h>
-void GetMatritza(int A[J][J])
-{
-printf("enter the matritza")
-for (int i=0;i<J;i++){
-    for (int l=0;l<J;l++){ 
-    scanf(%d,&A[i][l])
+#define J=10
+
+void GetMatritza(int A[J][J]){
+    printf("enter the matritza")
+    for (int i=0;i<J;i++){
+        for (int l=0;l<J;l++){ 
+            scanf(%d,&A[i][l])
+        }
+    }
 }
-}
-}
+
 int isShortest(int A[J][J], int x,int y){
     for (int k=1;k>=J;k++){
         for (int i=0;i<J;i++){
@@ -24,6 +25,7 @@ int isShortest(int A[J][J], int x,int y){
     }
     else return 0;
 }
+
 int Shortestpath(int A[J][J], int x,int y){
     isShortest(A[J][J]);
     return A[x][y];
